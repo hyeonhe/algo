1cd6126478975f67f8439adf19c3488bc13b705b
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2022-06-03 09:42:29 +0900

    2022/06/01
    solved 폴더로 옮김

--- solved/9012.py
+++ solved/9012.py
@@ -1,24 +1,23 @@
 import sys
-from collections import deque
 def input():
     return sys.stdin.readline().rstrip()
 
 t = int(input())
-queue = deque()
+stack = []
 
 for _ in range(t):
     str = input()
     for i in str:
         if i == '(':
-            queue.append(i)
+            stack.append(i)
         elif i == ')':
-            if queue and queue[-1] == '(':
-                queue.pop()
+            if stack and stack[-1] == '(':
+                stack.pop()
             else:
-                queue.append(i)
+                stack.append(i)
                 break
-    if queue:
+    if stack:
         print('NO')
     else:    
         print('YES')
-    queue = []
+    stack = []
