adb4af843daa27610d38cc6ea9f5ef4ff0587406
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2022-06-04 20:14:45 +0900

    2022/06/04
    2문제 풂

---
+++ solved/10809.py
@@ -0,0 +1,14 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+s = list(input())
+abc = [-1] * 26
+
+for i in range(len(s)):
+    if abc[ord(s[i]) - 97] == -1:
+        abc[ord(s[i]) - 97] = i
+
+for i in abc:
+    print(i, end=' ')
