24943b1b4e68fe211e07929c5c202d83f1cc5d83
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2022-06-21 16:18:12 +0900

    20220621
    일단 3문제 풀었다

---
+++ solved/4673.py
@@ -0,0 +1,12 @@
+ls10000 = set(range(1, 10001))
+generated_num = set()
+
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)
+    generated_num.add(i)
+
+self_num = sorted(ls10000 - generated_num)
+
+for i in self_num:
+    print(i)
