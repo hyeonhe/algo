15ebe8cff8e83f6e7feb4cf134b4d45ffc71d5c6
김현희_T4061 <63030323+hyeonhe@users.noreply.github.com>
2022-10-01 19:11:36 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,21 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+n = int(input())
+cnt = n
+for i in range(n):
+    word = input()
+    chr = {}
+    for j in range(len(word)):
+        if word[j] not in chr:
+            chr[word[j]] = j
+        elif chr[word[j]] + 1 == j:
+            chr[word[j]] = j
+        else:
+            cnt -= 1
+            break
+    chr = {}
+
+print(cnt)
