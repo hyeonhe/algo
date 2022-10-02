6c46383dbe068d954ff315f52a627866cd43481c
김현희_T4061 <63030323+hyeonhe@users.noreply.github.com>
2022-10-02 13:44:48 +0900

    [Bronze I] Title: 최소공배수, Time: 156 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,15 @@
+def gcd(x, y):
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
+
+ans = 1
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    c = gcd(a, b)
+    a //= c
+    b //= c
+    ans = a * b * c
+    print(ans)
