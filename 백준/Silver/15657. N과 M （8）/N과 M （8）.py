2087a192a1c0b69842dd863a9f03e7742cc4b699
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2022-12-21 12:26:42 +0900

    [Silver III] Title: N과 M (8), Time: 52 ms, Memory: 31956 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -5,12 +5,8 @@ def input():
     return sys.stdin.readline().rstrip()
 
 n, m = map(int, input().split())
-num = list(map(int, input().split()))
-num.sort()
-
-array = combinations_with_replacement(num, m)
+num = sorted(list(map(int, input().split())))
+array = sorted(set(combinations_with_replacement(num, m)))
 
 for i in array:
-    for j in i:
-        print(j, end=' ')
-    print()
+    print(' '.join(map(str, i)))
