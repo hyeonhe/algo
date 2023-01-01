e31772e3d5727410aab116efb0242c7314fbf58a
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-01-01 16:50:41 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 260 ms, Memory: 48388 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,15 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+sites = {}
+
+for _ in range(n):
+    site, pw = input().split()
+    sites[site] = pw
+
+for _ in range(m):
+    site = input()
+    print(sites[site])
