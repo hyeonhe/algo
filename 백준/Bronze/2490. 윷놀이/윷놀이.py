5bbdf94d1a22f1e759204d88712c96388ad3b8e0
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-01-14 16:36:14 +0900

    [Bronze III] Title: 윷놀이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,14 @@
+for _ in range(3):
+    array = list(map(int, input().split()))
+    a = array.count(0)
+
+    if a == 1:
+        print('A')
+    elif a == 2:
+        print('B')
+    elif a == 3:
+        print('C')
+    elif a == 4:
+        print('D')
+    else:
+        print('E')
