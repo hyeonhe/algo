84aab05176b7b060790196250951cc9b6c6febed
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-01-15 12:14:07 +0900

    [Silver I] Title: 1로 만들기 2, Time: 2364 ms, Memory: 377848 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -16,6 +16,6 @@ for i in range(2, n + 1):
         dp[i][0] = dp[i//2][0] + 1
         dp[i][1] = dp[i//2][1] + [i]
 
-array = sorted(dp[-1][1], reverse=True)
 print(dp[-1][0])
-print(' '.join(map(str, array)))
+for i in dp[-1][1][::-1]:
+    print(i, end=' ')
