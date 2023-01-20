2f2132200e7ab59420c05049710c24c99af5374b
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-01-20 22:05:12 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 39456 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -7,8 +7,7 @@ array = list(map(int, input().split()))
 first = sum(array[:k])
 ans_array = [first]
 
-ans = -101
-for i in range(1, n-k+1):
-    ans_array.append(ans_array[i-1] - array[i-1] + array[i+k-1])
+for i in range(n-k):
+    ans_array.append(ans_array[i] - array[i] + array[i+k])
 
 print(max(ans_array))
