14e1d7d4f5a2cf186993722c954cfa68e3cfb208
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-01-24 11:13:34 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -7,9 +7,7 @@ else:
     ans = 99
 
 for i in range(100, n+1):
-    num = str(i)
-    a, b, c = int(num[0]), int(num[1]), int(num[2])
-    if c - b == b - a:
+    if (i // 100) + (i % 10) == 2 * (i // 10 % 10):
         ans += 1
 
 print(ans)
