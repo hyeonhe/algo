199180f254c61e7eb4692f8503500a74ce23724e
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-01-29 11:06:16 +0900

    [Silver II] Title: 최대 힙, Time: 148 ms, Memory: 46208 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,19 +1,17 @@
-import heapq
 import sys
-
-def input():
-    return sys.stdin.readline().rstrip()
+import heapq
+input = sys.stdin.readline
 
 n = int(input())
 heap = []
 
 for _ in range(n):
     x = int(input())
-
-    if x:
-        heapq.heappush(heap, (-x, x))
-    else:
-        if len(heap) >= 1: 
-            print(heapq.heappop(heap)[1])
-        else:
+    if x == 0:
+        if len(heap) == 0:
             print(0)
+        else:
+            item = heapq.heappop(heap)
+            print(item[1])
+    else:
+        heapq.heappush(heap, (-x, x))
