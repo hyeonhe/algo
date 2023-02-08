e476211a9dd09b5fbe522483c6eefd02763dabe5
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-02-08 10:18:49 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,15 @@
+n = int(input())
+array = [[0] * 101 for _ in range(101)]
+ans = 0
+
+for _ in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            array[i][j] = 1
+
+for a in array:
+    ans += a.count(1)
+
+print(ans)
