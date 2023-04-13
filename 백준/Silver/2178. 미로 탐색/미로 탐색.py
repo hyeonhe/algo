cc46118eb1ded04c11b695180a9c7d492182dd77
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-13 21:11:19 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,31 @@
 from collections import deque
-import sys
-def input():
-    return sys.stdin.readline().rstrip()
 
-n, m = map(int, input().split())
-array = []
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-for i in range(n):
-    row = list(input())
-    array.append(row)
-
-def bfs(x, y):
+def bfs(graph, x, y):
     queue = deque()
-    queue.append((x, y))
-    array[x][y] = 1
+    queue.append([x, y])
 
     while queue:
         x, y = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+        
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if array[nx][ny] == '1':
-                queue.append((nx, ny))
-                array[nx][ny] = array[x][y] + 1
-            
+        
+            if graph[nx][ny] == 1:
+                queue.append([nx, ny])
+                graph[nx][ny] = graph[x][y] + 1
+
             if nx == n - 1 and ny == m - 1:
                 break
-    return array[n-1][m-1]
+    
+    return graph[n-1][m-1]
 
-print(bfs(0, 0))
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+print(bfs(graph, 0, 0))
