2907774d68bf72fe2201b1b9915bc56cf522c53d
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-16 16:27:55 +0900

    Update 안전 영역.py

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,27 +1,27 @@
 from collections import deque
- 
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
 n = int(input())
 graph = []
-maxNum = 0
- 
+max_num = 0
+
 for i in range(n):
     graph.append(list(map(int, input().split())))
-    for j in range(n):
-        if graph[i][j] > maxNum:
-            maxNum = graph[i][j] 
- 
- 
- 
+    max_num = max(max_num, max(graph[i]))
+
 dx = [0 ,0, 1, -1]
 dy = [1, -1, 0 ,0]
-def bfs(a, b, value, visited):
+def bfs(value, x, y, visited):
     q = deque()
-    q.append((a, b))
-    visited[a][b] = 1
- 
+    q.append((x, y))
+    visited[x][y] = 1
+
     while q:
         x, y = q.popleft()
- 
+        visited[x][y] = 1
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -29,20 +29,19 @@ def bfs(a, b, value, visited):
                 if graph[nx][ny] > value and visited[nx][ny] == 0:
                     visited[nx][ny] = 1
                     q.append((nx, ny))
- 
- 
-result = 0
-for i in range(maxNum): 
-    visited = [[0] * n for i in range(n)]
+    return 1
+
+ans = 0
+for i in range(max_num):
     cnt = 0
- 
+    visited = [[0] * n for i in range(n)]
+
     for j in range(n):
         for k in range(n):
-            if graph[j][k] > i and visited[j][k] == 0: 
-                bfs(j, k, i, visited)
-                cnt += 1
- 
-    if result < cnt:
-        result = cnt
- 
-print(result)
+            if graph[j][k] > i and visited[j][k] == 0:
+                cnt += bfs(i, j, k, visited)
+
+    if ans < cnt:
+        ans = cnt
+
+print(ans)
