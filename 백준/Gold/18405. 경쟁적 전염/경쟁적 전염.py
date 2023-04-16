03a6d7844cc6d95fd769bc7db5df5c60135aa844
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-16 15:02:27 +0900

    [Gold V] Title: 경쟁적 전염, Time: 112 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -9,9 +9,6 @@ n, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 S, X, Y = map(int, input().split())
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
 queue = []
 for i in range(n):
     for j in range(n):
@@ -29,18 +26,16 @@ def bfs():
         for _ in range(len(q)):
             num, x, y = q.popleft()
 
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+            for dx, dy in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                nx = x + dx
+                ny = y + dy
 
                 if nx < 0 or nx >= n or ny < 0 or ny >= n:
                     continue
                 if graph[nx][ny] == 0:
                     graph[nx][ny] = graph[x][y]
                     q.append((num, nx, ny))
-
         cnt += 1
-
     return graph[X-1][Y-1]
 
 print(bfs())
