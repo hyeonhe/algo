eba1eda373e87e72a881744f19061afa06d3c358
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-04-28 10:43:22 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -5,11 +5,8 @@ def solution(s):
     for i in range(len(s)):
         if i == 0:
             answer += s[i].upper()
-        elif i == len(s) - 1:
-            answer += s[i]
         elif s[i-1] == ' ':
             answer += s[i].upper()
-        
         else:
             answer += s[i]
             
