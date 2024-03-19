ab81315f0f89d5ef57ae7996a5aea9c0615350ec
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-03-19 16:33:14 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 124 ms, Memory: 117056 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,10 +1,17 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
 t = int(input())
-arr = list(int(input()) for i in range(t))
+arr = list(int(input()) for _ in range(t))
 
-dp=[0,1,2,4]
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-for i in range(4, max(arr)+1):
-    dp.append((dp[i-1] + dp[i-2] +dp[i-3]) % 1000000009)
+for i in range(4, 1000001):
+    dp[i] = ((dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009)
     
 for i in arr:
     print(dp[i])
